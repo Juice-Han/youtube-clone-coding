eb313cf917ac46c7b3cfbb8567d80b6555d7d48a
JuHan Park <iiiamst16@gmail.com>
2026-04-14 17:40:33 +0900

    리액트 라우터 설치 및 검색 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import Header from '@/components/header'
 import { ColorModeButton } from '@/components/ui/color-mode'
 import MainPage from '@/pages/main-page'
+import SearchPage from '@/pages/search-page'
 import { Box } from '@chakra-ui/react'
+import { Route, Routes } from 'react-router'
 
 const App = () => {
   return (
     <Box mt={112}>
       <Header />
-      <MainPage />
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='/search' element={<SearchPage />} />
+      </Routes>
       <ColorModeButton />
     </Box>
   )
