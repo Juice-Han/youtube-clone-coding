901414580b416857d3944d91b2c555db54247d08
JuHan Park <iiiamst16@gmail.com>
2026-04-14 23:09:18 +0900

    번들 파일 분리

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,21 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/react') || id.includes('node_modules/react-dom') || id.includes('node_modules/react-router')) {
+            return 'react-vendor'
+          }
+          if (id.includes('node_modules/@chakra-ui') || id.includes('node_modules/@emotion')) {
+            return 'chakra-vendor'
+          }
+          if (id.includes('node_modules/react-icons')) {
+            return 'icons-vendor'
+          }
+        },
+      },
+    },
+  },
 })
