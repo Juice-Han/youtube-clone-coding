73c18fd69f12bda6755de2d9326c6bc1071b9753
JuHan Park <iiiamst16@gmail.com>
2026-04-15 17:11:23 +0900

    검색 결과 없을 때 보여줄 UI 구현

--- src/pages/search-page.tsx
+++ src/pages/search-page.tsx
@@ -1,7 +1,7 @@
 import VideoSearchResult from '@/components/video-search-result'
 import VideoThumbnail from '@/components/video-thumbnail'
 import { thumbnailMockData } from '@/mock/thumbnail-mock'
-import { Box, Flex } from '@chakra-ui/react'
+import { Box, Center, Flex, Text } from '@chakra-ui/react'
 import { useNavigate, useSearchParams } from 'react-router'
 
 const SearchPage = () => {
@@ -15,17 +15,21 @@ const SearchPage = () => {
 
   const filteredVideoData = thumbnailMockData.filter((v) => v.title.includes(q!))
 
+  if (filteredVideoData.length === 0) {
+    return (
+      <Center pt={3}>
+        <Text>검색 결과가 존재하지 않습니다.</Text>
+      </Center>
+    )
+  }
+
   return (
     <>
       <Flex direction={'column'} p={4} maxW={'1250px'} mx={'auto'}>
         {filteredVideoData.map((data, idx) => (
           <Box key={`${data.title} 영상`}>
             <Box display={{ base: 'block', sm: 'none' }}>
-              <VideoThumbnail
-                lazyLoading={idx > 6 ? true : false}
-                fetchPriority={idx < 3 ? true : false}
-                {...data}
-              />
+              <VideoThumbnail lazyLoading={idx > 6 ? true : false} fetchPriority={idx < 3 ? true : false} {...data} />
             </Box>
             <Box display={{ base: 'none', sm: 'block' }}>
               <VideoSearchResult
